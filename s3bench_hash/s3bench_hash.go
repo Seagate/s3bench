351a02dbd619b6cb70dd85e72a07815781fddaf3
Dmitrii Surnin <dmitrii.surnin@seagate.com>
2021-01-19 23:59:41 -0700

    Add s3bensh_hash
    Signed-off-by: Dmitrii Surnin <dmitrii.surnin@seagate.com>

---
+++ s3bench_hash/s3bench_hash.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+	"io"
+	"fmt"
+	"crypto/sha512"
+	"encoding/base32"
+
+)
+
+func to_b32(dt []byte) string {
+	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(dt)
+}
+
+func main() {
+	file := flag.String("file", "file.ext", "file to read")
+	flag.Parse()
+
+	f, err := os.Open(*file)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	hasher := sha512.New()
+	reader := bufio.NewReader(f)
+	sz, err := io.Copy(hasher, reader)
+	sum := hasher.Sum(nil)
+	hash_base32 := to_b32(sum[:])
+
+	fmt.Printf("File %v \nSize %v \nS3Bench Hash %v \n", *file, sz, hash_base32)
+
+}
